df7204fe6f11ed03cc277ee1b23748d13ee1c782
zbl <786028121@qq.com>
2016-07-25 11:02:25 +0800

    list

--- src/sass/js/list.js
+++ src/sass/js/list.js
@@ -6,7 +6,9 @@ $(function(){
 	var $hide=$('.hide');
 	var $bgimg=$('.bgimg');
 	var $hide_more=$('.hide_more');
-	var $more=$('.more')	
+	var $more=$('.more');
+	var $baijiu=$('.baijiu');
+	var $hide1=$('.hide1');
 	$putao.on('click',function(){
 		
 		$hide.toggle();
@@ -14,5 +16,9 @@ $(function(){
 	})
 	$more.on('click',function(){
 		$hide_more.toggle()
+	})
+	$baijiu.on('click',function(){
+		$hide1.toggle();
+		
 	})
 })
